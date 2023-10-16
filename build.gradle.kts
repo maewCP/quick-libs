92656fbc1525d768cc71b80b0bd620170276d962
Teeraphab Laomettachit <maew_group_r@hotmail.com>
2023-10-16 22:11:48 +0700

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    kotlin("jvm") version("1.9.0")
+    `maven-publish`
+}
+
+group = "net.maew"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "net.maew"
+            artifactId = "quick-libs"
+            version = "1.0-SNAPSHOT"
+
+            from(components["java"])
+        }
+    }
+}
+
+kotlin {
+    jvmToolchain(8)
+}
