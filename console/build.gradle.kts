a8fb9f17104412f4e773a5b2df4f2bfc522c14e4
Teeraphab Laomettachit <maew_group_r@hotmail.com>
2023-10-16 22:45:36 +0700

    make jitpack works

--- console/build.gradle.kts
+++ console/build.gradle.kts
@@ -1,12 +1,40 @@
 plugins {
     kotlin("jvm")
+    `maven-publish`
 }
-dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-}
+
+group = "net.maew.quick-libs"
+version = "1.0-SNAPSHOT"
+
 repositories {
     mavenCentral()
 }
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "net.maew.quick-libs"
+            artifactId = "console"
+            version = "1.0-SNAPSHOT"
+
+            from(components["java"])
+        }
+    }
+}
+
 kotlin {
     jvmToolchain(8)
 }
